ee5dff8bb85d7c8fbd1c36611c6414b94457e7ba
Alex Lew <alew@caltech.edu>
2016-03-03 21:56:07 -0800

    First commit, added readme

--- player.cpp
+++ player.cpp
@@ -39,5 +39,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */ 
+
+    std::cerr << "first commit!" << std::endl;
     return NULL;
 }
